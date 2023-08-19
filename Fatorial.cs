f1d78758db1d8a53465a9c911a327a5efc3afdbc
Philippe <philippedsantos@hotmail.com>
2023-08-19 17:33:30 -0300

    first commit

---
+++ Fatorial.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    public static int Fatorial(int n)
+    {
+        if (n == 0)
+            return 1;
+        else
+            return n * Fatorial(n - 1);
+    }
+    private static void Main(string[] args)
+    {
+
+    }
+}
